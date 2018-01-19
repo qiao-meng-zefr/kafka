5a816822b5cc405cf55d8e5a5931cba42c88b7ee
Robin Tang <tang8330@mylaurier.ca>
2018-01-19 09:35:40 -0800

    no use for connType

--- connect/runtime/src/main/java/org/apache/kafka/connect/runtime/Herder.java
+++ connect/runtime/src/main/java/org/apache/kafka/connect/runtime/Herder.java
@@ -16,7 +16,6 @@
  */
 package org.apache.kafka.connect.runtime;
 
-import org.apache.kafka.common.config.ConfigDef;
 import org.apache.kafka.connect.runtime.isolation.Plugins;
 import org.apache.kafka.connect.runtime.rest.entities.ConfigInfos;
 import org.apache.kafka.connect.runtime.rest.entities.ConnectorInfo;
@@ -172,11 +171,10 @@ public interface Herder {
 
     /**
      * Goes through config parameters and replace password field value with "[hidden"]
-     * @param connName name of the connector
      * @param config configuration of the connector
      * @return new map of the configurations, with password omitted from clear-text
      */
-    Map<String, String> maskCredentials(String connName, Map<String, String> connectorInfo);
+    Map<String, String> maskCredentials(Map<String, String> config);
 
     /**
      * Returns a handle to the plugin factory used by this herder and its worker.
